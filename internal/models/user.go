ced2c2b70652ff6017adbd2e6594f0973b5c2cc5
yaren <yarengokhn@gmail.com>
2026-01-23 22:57:39 +0200

    comment added

--- internal/models/user.go
+++ internal/models/user.go
@@ -2,10 +2,11 @@ package models
 
 type User struct {
 	ID           uint          `gorm:"primaryKey" json:"id"`
-	FirstName    string        `gorm:"not null" json:"first_name"`
-	LastName     string        `gorm:"not null" json:"last_name"`
-	Email        string        `gorm:"unique;not null" json:"email"`
-	Password     string        `gorm:"not null" json:"-"`
+	FirstName    string        `gorm:"not null" json:"first_name"`   //mandotary fields
+	LastName     string        `gorm:"not null" json:"last_name"`    //mandotary fields
+	Email        string        `gorm:"unique;not null" json:"email"` //mandotary fields
+	Password     string        `gorm:"not null" json:"-"`            //mandotary fields,hides from json
 	Trips        []Trip        `gorm:"foreignKey:UserID" json:"trips,omitempty"`
 	ChatMessages []ChatMessage `gorm:"foreignKey:UserID" json:"chat_messages,omitempty"`
+	//omitempty :if this field is empty it won't be shown in the json
 }
